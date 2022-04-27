fa7c86e38aa905e9c58711f3ca02f49227c845bc
xiaoxunyao <huchengyea@163.com>
2022-04-27 18:49:02 +0800

    fix: `class` to `function`

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React,{useState} from 'react'
 import { BiSun,BiMoon } from "react-icons/bi"
 import { GoMarkGithub } from "react-icons/go";
 
@@ -10,37 +10,26 @@ function icons(mode:boolean){
   }
 }
 
-class Footer extends React.Component<{}, {mode: boolean}>{
+function Footer(){
 
-  constructor(props:any){
-    super(props)
-    this.state = {
-      mode:false
-    }
+  const [mode,setMode] = useState(false)
 
+
+  const dark = () => {
+    const darkMode = document.documentElement.classList.toggle('dark')
+    setMode(darkMode)
   }
 
-  dark = () =>{
-    const darkMode:boolean = document.documentElement.classList.toggle('dark')
-    this.setState({
-      mode:darkMode
-    })
-  }
-
-  render(){
-    
-    return (
+  return(
       <nav className="text-xl mt-6 inline-flex gap-2">
-        <button onClick={this.dark} className="text-[0.9em] inline-block cursor-pointer select-none opacity-75 transition duration-200 ease-in-out !outline-none hover:opacity-100 hover:text-teal-600">
-          {icons(this.state.mode)}
+        <button onClick={dark} className="text-[0.9em] inline-block cursor-pointer select-none opacity-75 transition duration-200 ease-in-out !outline-none hover:opacity-100 hover:text-teal-600">
+          {icons(mode)}
         </button>
         <a href="https://github.com/elonehoo/ritem-mini" target="_blank" className="text-[0.9em] inline-block cursor-pointer select-none opacity-75 transition duration-200 ease-in-out !outline-none hover:opacity-100 hover:text-teal-600">
           <GoMarkGithub />
         </a>
       </nav>
-    )
-  }
-  
+  )
 }
 
 export default Footer
--- src/pages/all/all.tsx
+++ src/pages/all/all.tsx
@@ -1,18 +1,11 @@
 import React from "react"
 
-class All extends React.Component{
-  constructor(props:any){
-    super(props)
-
-  }
-
-  render(){
-    return (
-      <div>
-        Not Found
-      </div>
-    )
-  }
+function All(){
+  return (
+    <div>
+      Not Found
+    </div>
+  )
 }
 
 export default All
